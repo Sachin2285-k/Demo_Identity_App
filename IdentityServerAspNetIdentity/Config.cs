45f56393d5b9046c08b467df36999ebb2c1296a4
Sachin2285-k <sachin14422@gmail.com>
2023-10-18 17:54:39 +0530

    Initial Setup

---
+++ IdentityServerAspNetIdentity/Config.cs
@@ -0,0 +1,74 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using IdentityModel;
+
+namespace IdentityServerAspNetIdentity;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResource()
+            {
+                Name = "verification",
+                UserClaims = new List<string>
+                {
+                    JwtClaimTypes.Email,
+                    JwtClaimTypes.EmailVerified
+                }
+            },
+            new IdentityResource("color", new [] {"favorite_color"})
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            {
+            new ApiScope(name: "api1", displayName: "MyAPI")
+            };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+            {
+
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret ("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                },
+
+                new Client
+                {
+                    ClientId = "web",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "verification",
+                        "api1",
+                        "color"
+                    }
+                }
+
+            };
+}
